02a3f7b321bbadfdb6d9bee9fc7f9622c2ef9bba
Rajdeep <89543670+rrajdeep@users.noreply.github.com>
2022-09-04 22:14:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Accessing elements by Id and adding content
+document.getElementById('heading1').innerHTML = "Hello World!";
+document.getElementById('heading2').innerHTML = "Introduction to JavaScript";
+document.getElementById('paragraph').innerHTML = "This is JavaScript Assignment.";
+
+// Accessing elements by Id and adding style
+document.getElementById('heading2').style.color = "blue";
+document.getElementById('paragraph').style.color = "red";
+
+// Accessing whole body and adding background color
+document.body.style.backgroundColor = "yellow";
